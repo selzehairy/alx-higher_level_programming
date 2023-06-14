c26bf860da3785b2a071d2b26a0a461e67175815
selzehairy <selzehairy@gmail.com>
2023-06-13 17:09:34 -0700

    add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+const firstInteger = parseInt(process.argv[2]);
+const secondInteger = parseInt(process.argv[3]);
+add(firstInteger, secondInteger);
